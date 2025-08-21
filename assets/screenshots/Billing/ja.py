c113111b2eec8287f99bdd625a044eb72de9ad16
dr4xven <amonmwagala@gmail.com>
2025-08-21 09:11:32 +0300

    chore: update posts

--- assets/screenshots/Billing/ja.py
+++
@@ -1,9 +0,0 @@
-lifetime = datetime.datetime.now() + datetime.timedelta(minutes=5)
-
-payload = {
-    'username' : username,
-    'admin' : 1,
-    'exp' : lifetime
-}
-
-access_token = jwt.encode(payload, self.secret, algorithm="HS256")
